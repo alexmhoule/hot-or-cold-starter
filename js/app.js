1695b2974d8380452f6b9103a632e8ab9456b261
alexmhoule <alexmhoule@gmail.com>
2015-05-12 00:02:06 -0500

    random number worked user input not so much

--- js/app.js
+++ js/app.js
@@ -12,6 +12,27 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Start New Game ---*/
+
+  	var hiddenNumber = Math.ceil(Math.random()*100);
+    var userGuess = $('#userGuess').val();
+    userGuessAsInput = parseInt(userGuess);
+    var guessDiff = Math.abs(hiddenNumber - userGuessAsInput);
+
+    /*--- Checking to see if user is hot or cold ---*/
+  	
+     $('#guessButton').click(function () {
+        if (isNaN(userGuessAsInput)==true) {
+          alert("Please enter a number");
+        } else if (guessDiff <= 5) { 
+            alert("hot");
+        } else if (guessDiff <= 10) {
+            alert("warm");
+        } else 
+          alert("cold");
+
+    });
+
 });
 
 
